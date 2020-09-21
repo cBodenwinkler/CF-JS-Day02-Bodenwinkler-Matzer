656211ad956eaa15cb3d3e87e9c948f2874f3de2
LeDaniii <danielmatzer@gmail.com>
2020-09-21 11:03:25 +0200

    exercise2

---
+++ basic2.js
@@ -0,0 +1,15 @@
+let number = [1, 7, 10, 9, 55];
+
+let arrLen = number.length;
+
+// console.log(arrLen);
+
+let counter = 0;
+
+for (i = 0; i < arrLen; i++) {
+  if (number[i] >= counter) {
+    counter = number[i];
+  }
+}
+
+console.log(counter);
